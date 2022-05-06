19a11558c45b8d8591fa5250ed4d7c7b6ce0f5f6
anish <anishbishnoi127@gmail.com>
2022-05-07 01:17:18 +0530

    new functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
+// import Header from './components/common/header';
+import Header from './components/common/header/index';
+
 import {Home} from './pages/Home';
 const App = () => {
   return (
-  <Home/>
+    <>
+    <Header/>
+    <Home/>
+  </>
   )
 }
 
--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -4,6 +4,15 @@ import './heroSection.css';
 const HeroSection = () => {
   return (
     <div className="hero-section-wrapper ">
+
+<div className="flex absolute-center hero-claim-label">
+  <div>pay credit card bill. earn guaranteed ₹200 back. </div>
+<div className="claim-anchor">
+ claim now 
+<img src="https://web-images.credcdn.in/_next/assets/images/home-page/arrow.png" className="claim-arrow" alt="arrow"/>
+</div>
+</div>
+    
       <div className="flex absolute-center flex-col hero-section max-width">
         <div className="hero-heading">
           rewards for paying credit card bills.
