bd9cb0d0364032efebf9e1a1f88eeeab53bbbe81
anish <anishbishnoi127@gmail.com>
2022-05-26 18:38:02 +0530

    new features added components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,16 @@ import React from 'react'
 import HeroSection from '../components/HeroSection'
 import ProductShowcase from '../components/ProductShowcase'
 import Header from '../components/common/header'
+import FeelSpecial from '../components/FeelSpecial'
+import CredExperience from '../components/CredExperience'
 export const Home = () => {
   return (
    <>
    <Header/>
    <HeroSection />
-   <ProductShowcase /></>
+   <ProductShowcase />
+   <FeelSpecial/>
+   <CredExperience/>
+   </>
   )
 }
