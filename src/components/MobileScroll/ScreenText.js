3516f798b463e62e3a8485dc1a372e59bed190e8
anish <anishbishnoi127@gmail.com>
2022-05-26 21:49:11 +0530

    featues added

--- src/components/MobileScroll/ScreenText.js
+++ src/components/MobileScroll/ScreenText.js
@@ -1,30 +1,31 @@
 import React,{useEffect,useRef,useState} from 'react'
 
 const ScreenText = ({screen,setCurrentImg,i}) => {
-    const [showAnimation,setShowAnimation] = useState(false);
     const ref = useRef(null);
+    const [showAnimation,setShowAnimation] = useState(false);
 
     const toggleAnimation = (e)=>{
         if(e[0].isIntersecting){
             setShowAnimation(true);
+            setCurrentImg(i);
         }
     }
     const options={
-root:null,
-rootMargin:'0px',
-threshold:0.6,
+        root:null,
+        rootMargin:'0px',
+        threshold:0.6,
     }
     useEffect(()=>{
     const observer = new IntersectionObserver(toggleAnimation,options);
-if(!showAnimation){
+// if(!showAnimation){
     if(ref.current){
         observer.observe(ref.current);
     }
     return ()=>{
      observer.unobserve(ref.current);   
     }
-}
-})
+// }
+},[]);
   return (
     <div className={`screen-text ${showAnimation ? "text-visible":""}`} ref={ref}>
     <div className="screen-heading">{screen.heading}</div>
