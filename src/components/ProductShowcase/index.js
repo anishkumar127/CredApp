885662ed28f8a71fe8c0c2fae7b302bd0a01e24e
anish <anishbishnoi127@gmail.com>
2022-05-26 20:50:06 +0530

    featues added

--- src/components/ProductShowcase/index.js
+++ src/components/ProductShowcase/index.js
@@ -31,7 +31,7 @@ if(!showAnimation){
     >
 {   showAnimation &&(   <div className="showcase-wrapper">
 <img src="https://web-images.credcdn.in/_next/assets/images/home-page/phone/left-2.png" className="showcase-ui showcase-mockup-1" alt="Product showcase"/>
-<img src="https://web-images.credcdn.in/_next/assets/images/home-page/phone/left1.png" className="showcase-ui showcase-mockup-2" alt="Product showcase"/>
+<img src="https://web-images.credcdn.in/_next/assets/images/home-page/phone/left-1.png" className="showcase-ui showcase-mockup-2" alt="Product showcase"/>
 <img src="https://web-images.credcdn.in/_next/assets/images/home-page/phone/center.png" className="showcase-ui showcase-mockup-3" alt="Product showcase"/>
 <img src="https://web-images.credcdn.in/_next/assets/images/home-page/phone/right-1.png" className="showcase-ui showcase-mockup-4" alt="Product showcase"/>
 <img src="https://web-images.credcdn.in/_next/assets/images/home-page/phone/right-2.png" className="showcase-ui showcase-mockup-5" alt="Product showcase"/>
