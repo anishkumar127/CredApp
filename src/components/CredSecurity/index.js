fb37d962526ec489994812cc4a0d4b57075da30f
anish <anishbishnoi127@gmail.com>
2022-05-26 19:02:11 +0530

    changes and features included

--- src/components/CredSecurity/index.js
+++ src/components/CredSecurity/index.js
@@ -1,8 +1,33 @@
 import React from 'react'
-
+import Button from '../common/Button';
+import './credSecurity.css';
 const CredSecurity = () => {
   return (
-    <div></div>
+    <div className="cred-security photo-section">
+    <div className="max-width">
+        <div className="phot-section-child">
+            <div className="photo-section-top">
+                <div className="photo-section-heading">
+                    feel special more often.
+                </div>
+                <div className="photo-section-subheading">
+                exclusive rewards for paying your bills
+                </div>
+            </div>
+            <div className="photo-section-bottom">
+                <div className="photo-section-description">
+                every time you pay your credit card bills on CRED, you receive CRED coins. you can use these to win exclusive rewards or get special access to curated products and experiences. on CRED, good begets good.
+
+                </div>
+                <div>
+                    <Button buttonText="Explore rewards"/>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    </div>
   )
 }
 
